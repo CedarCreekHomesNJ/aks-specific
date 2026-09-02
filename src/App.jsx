e56c8440bb48e81ba265cb5d98d3c721b5f4e89b
Ahmet Kamil <ahmet.cedarcreek@outlook.com>
2026-09-02 15:42:59 -0400

    Retire legacy coach-profile feature, seal its database tables

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import TeamDashboard from './pages/TeamDashboard'
-import CoachProfile from './pages/CoachProfile'
 import Login from './pages/Login'
 
 export default function App() {
@@ -9,7 +8,6 @@ export default function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/t/:code" element={<TeamDashboard />} />
-      <Route path="/p/:code" element={<CoachProfile />} />
       <Route path="/login" element={<Login />} />
     </Routes>
   )
